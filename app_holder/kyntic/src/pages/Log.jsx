3586472fd9b3bde8b09ed7477bd296ffa1e78572
TheEnquirer <huxmarv@nuevaschool.org>
2021-06-24 21:49:10 -0700

    log page

--- app_holder/kyntic/src/pages/Log.jsx
+++ app_holder/kyntic/src/pages/Log.jsx
@@ -1,42 +1,85 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonRange, IonIcon,
-    IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent
+    IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonInput, IonTextarea, IonLabel
 } from '@ionic/react';
-import { happy, sad } from 'ionicons/icons';
+import { happy, sad, bed, batteryDead, batteryCharging } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import './Log.css';
+import { Controlled as CodeMirror } from 'react-codemirror2'
+import React, { useState } from "react"
 
 const Log = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Log</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-	    <IonTitle size="large">Log</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-	  {
-	  //<ExploreContainer name="Tab 1 page" />
-	  }
+    const [text, setText] = useState("")
+    return (
+	<IonPage>
+	    <IonHeader>
+		<IonToolbar>
+		    <IonTitle>Log</IonTitle>
+		</IonToolbar>
+	    </IonHeader>
+	    <IonContent fullscreen>
+		<IonHeader collapse="condense">
+		    <IonToolbar>
+			<IonTitle size="large">Log</IonTitle>
+		    </IonToolbar>
+		</IonHeader>
 
-	  <IonCard>
-	      <IonCardHeader>
-		  <IonCardSubtitle>How are you feeling?</IonCardSubtitle>
-		  <IonCardTitle>Mood</IonCardTitle>
-	      </IonCardHeader>
-	      <IonItem>
-		  <IonRange min={0} max={100} step={1} snaps={false} color="tertiary" />
-		  <IonIcon slot="start" icon={sad} />
-		  <IonIcon slot="end" icon={happy} />
-	      </IonItem>
-	  </IonCard>
-      </IonContent>
-    </IonPage>
-  );
+		<IonCard>
+		    <IonCardHeader>
+			<IonCardSubtitle>How are you feeling?</IonCardSubtitle>
+			<IonCardTitle>Mood</IonCardTitle>
+		    </IonCardHeader>
+		    <IonItem>
+			<IonRange min={0} max={100} step={1} snaps={false} color="tertiary" />
+			<IonIcon slot="start" icon={sad} />
+			<IonIcon slot="end" icon={happy} />
+		    </IonItem>
+		</IonCard>
+
+
+		<IonCard>
+		    <IonCardHeader>
+			<IonCardSubtitle>How many hours of sleep did you get?</IonCardSubtitle>
+			<IonCardTitle>Sleep</IonCardTitle>
+		    </IonCardHeader>
+		    <IonItem>
+			<IonRange min={0} max={10} step={1} pin={true} snaps={true} color="secondary" />
+			<IonIcon size="small" slot="start" icon={bed} />
+			<IonIcon slot="end" icon={bed} />
+		    </IonItem>
+		</IonCard>
+
+		<IonCard>
+		    <IonCardHeader>
+			<IonCardSubtitle>How much energy do you have?</IonCardSubtitle>
+			<IonCardTitle>Energy</IonCardTitle>
+		    </IonCardHeader>
+		    <IonItem>
+			<IonRange min={0} max={100} step={1} snaps={false} color="warning" />
+			<IonIcon slot="start" icon={batteryDead} />
+			<IonIcon slot="end" icon={batteryCharging} />
+		    </IonItem>
+		</IonCard>
+
+		<IonCard>
+		    <IonCardHeader>
+			<IonCardSubtitle>What happened today?</IonCardSubtitle>
+			<IonCardTitle>Events</IonCardTitle>
+		    </IonCardHeader>
+		    <IonItem>
+			<IonTextarea
+			    placeholder="Description..."
+			    value={text}
+			    onIonChange={e => setText(e.detail.value)}
+			    autoGrow={true}
+			    spellCheck={true}
+			    className="input"
+			></IonTextarea>
+		    </IonItem>
+		</IonCard>
+
+	    </IonContent>
+	</IonPage>
+    );
 };
 
 export default Log;
