c0b3f27200caab93c1a9199b93090dde1a383931
Huxley Marvit <huxmarv@nuevaschool.org>
2022-01-10 00:03:59 +0000

    sliding to target

--- v2/components/pages/Sub.jsx
+++ v2/components/pages/Sub.jsx
@@ -9,23 +9,39 @@ import {
   IonContent,
   IonMenuButton,
 } from '@ionic/react';
-import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import Mood from "../ui/Mood"
+import React, { useState, useRef, useEffect } from 'react';
+import { Controller } from 'swiper';
+
 
 
 const Sub = () => {
+    useEffect(() => {
+	if (swiperRef.current) {
+	    swiperRef.current.swiper.slideTo(2);
+	}
+    })
+
+    const [controlledSwiper, setControlledSwiper] = useState(null);
+    const swiperRef = useRef(null)
+
     return (
 	<IonPage> <IonContent className="" fullscreen>
 	    <Swiper className="absolute h-screen border-0 border-blue-500"
+		modules={[Controller]} 
+		ref={swiperRef}
+		//controller={{ control: controlledSwiper }}
+		onSwipe ={setControlledSwiper}
 		spaceBetween={0}
 		slidesPerView={1}
 		onSlideChange={() => console.log('slide change')}
-		onSwiper={(swiper) => console.log(swiper)}
+		//onSwiper={(swiper) => console.log(swiper, "yooo")}
 		hashNavigation={{ replaceState: true }}
 		history={true}
 	    >
+		{console.log(controlledSwiper)}
 		<SwiperSlide> <Mood /> </SwiperSlide>
 		<SwiperSlide> <Mood /> </SwiperSlide>
 		<SwiperSlide>Slide 3</SwiperSlide>
@@ -36,3 +52,4 @@ const Sub = () => {
     );
 };
 export default Sub;
+
