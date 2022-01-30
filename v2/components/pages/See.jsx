7cadd67ff6e69f964551ec0a58d07758f1fd11a9
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2022-01-29 19:00:28 -0800

    I tried.

--- v2/components/pages/See.jsx
+++ v2/components/pages/See.jsx
@@ -12,13 +12,17 @@ import {
 import { useState } from 'react';
 import pageStyles from '../../styles/Pages.module.css';
 import { MetawearCapacitor } from 'metawear-capacitor';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 let connectedListenerMade = false; // when we have made a listener to listen if we have successfully connected 
 let connectCalled = false; // have we asked the plugin to connect?
 let connected = false; // have we been told by the plugin that we have successfully connected?
 let startedLogging = false; // have we started to log data?
 
-
+/**
+ * Creates a listener to see if we have successfully connected to the sensor.
+ * If we have, call the startLogging function.
+ */
 const createConnectedListener = () => {
 	if (!connectedListenerMade) {
 		connectedListenerMade = true;
@@ -33,38 +37,57 @@ const createConnectedListener = () => {
 	}
 }
 
+/**
+ * Attempts to create the data files.
+ * If we successfully create the files, start logging.
+ */
 const startLogging = () => {
 	if (!startedLogging) {
 		startedLogging = true;
 
 		MetawearCapacitor.createDataFiles().then((successful) => {
-			if (successful.successful)
-			{
-				console.log('Data files created!');
+			if (successful.successful) {
+				console.log('JS: Data files created!');
 				MetawearCapacitor.startData().then(() => {
-					console.log('Running startData did not error.');
+					console.log('JS: Running startData did not error.');
 				}).catch(err => {
-					console.log("Error while starting data logging:")
+					console.log("JS: Error while starting data logging:")
 					console.error(err);
 				});
 			}
-			else
-			{
-				console.log("Error while making data files.");
+			else {
+				console.log("JS: Error while making data files.");
 			}
 		}).catch(err => {
-			console.log("Error while making data files:")
+			console.log("JS: Error while making data files:")
 			console.error(err);
 		});
 	}
 }
 
+const writeFile = () => {
+	console.log("JS: gonna try to write fileeee")
+	return Filesystem.writeFile({
+		path: 'accel.txt',
+		data: "test",
+		directory: Directory.Documents,
+		encoding: Encoding.UTF8,
+	});
+};
+
 const See = () => {
 	// connect to the sensor
 	if (!connectCalled) {
+		// this just crashes the app...
+		// writeFile.then(() => {
+		// 	console.log("lets gooooo")
+		// }).catch(err => {
+		// 	console.log("Error while trying to use cap file plugin:")
+		// 	console.log(err);
+		// });
 		MetawearCapacitor.connect()
 			.then(async () => {
-				console.log('Running connection did not error.');
+				console.log('JS: Running connection did not error.');
 			})
 			.catch(err => {
 				console.error(err);
