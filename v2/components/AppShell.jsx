85344d3a686c57f7d02a38d09911466fc4c7cd6d
Huxley Marvit <huxmarv@nuevaschool.org>
2022-02-01 06:32:31 +0000

    some auth? idk

--- v2/components/AppShell.jsx
+++ v2/components/AppShell.jsx
@@ -1,36 +1,40 @@
 import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
 import { StatusBar, Style } from '@capacitor/status-bar';
+import Link from 'next/link'
 
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 import Menu from './Menu';
 
 import Tabs from './pages/Tabs';
+import supabase from '../lib/supabase'
 
 window.matchMedia('(prefers-color-scheme: dark)').addListener(async status => {
-	try {
-		await StatusBar.setStyle({
-			style: status.matches ? Style.Dark : Style.Light,
-		});
-	} catch { }
+    try {
+	await StatusBar.setStyle({
+	    style: status.matches ? Style.Dark : Style.Light,
+	});
+    } catch { }
 });
 
 const AppShell = () => {
-	//localStorage.theme = 'light'
+    const user = supabase.auth.user()
+    //localStorage.theme = 'light'
 
-	return (
-		<IonApp>
-			<IonReactRouter>
-				<IonSplitPane contentId="main">
-					{/*<Menu />*/}
-					<IonRouterOutlet id="main">
-						<Route path="/tabs" render={() => <Tabs />} />
-						<Route exact path="/" render={() => <Redirect to="/tabs" />} />
-					</IonRouterOutlet>
-				</IonSplitPane>
-			</IonReactRouter>
-		</IonApp>
-	);
+    return (
+	<IonApp>
+	    <IonReactRouter>
+		<IonSplitPane contentId="main">
+		    {/*<Menu />*/}
+		    <IonRouterOutlet id="main">
+			<Route path="/tabs" render={() => <Tabs />} />
+			<Route exact path="/" render={() => <Redirect to="/tabs" />} />
+		    </IonRouterOutlet>
+		          <Link href="/auth">Log In Now!</Link>
+		</IonSplitPane>
+	    </IonReactRouter>
+	</IonApp>
+    );
 };
 
 export default AppShell;
---
+++ v2/pages/auth.jsx
@@ -0,0 +1,47 @@
+import supabase from '../lib/supabase'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+
+
+const Auth = () => {
+    const [email, setEmail] = useState()
+    const { push } = useRouter()
+
+    const handleSubmit = async (e) => {
+	e.preventDefault()
+
+	const { error } = await supabase.auth.signIn({ email })
+
+	if (!error) push('/')
+    }
+
+    return (
+	<>
+	    <div className="p-12 mx-auto border rounded-lg">
+		<h3 className="text-3xl font-extrabold">Ahoy!</h3>
+
+		<p className="mt-4 text-sm text-gray-500">
+		    Fill in your email, we'll send you a magic link.
+		</p>
+
+		<form>
+		    <input
+			type="email"
+			placeholder="Your email address"
+			className="w-full p-3 mt-4 border rounded-lg focus:border-indigo-500"
+			onChange={e => setEmail(e.target.value)}
+		    />
+
+		    <button
+			type="submit"
+			className="w-full p-3 mt-8 text-white bg-indigo-500 rounded-lg hover:bg-indigo-700"
+		    >
+			Let's go!
+		    </button>
+		</form>
+	    </div>
+	</>
+  )
+}
+
+export default Auth
