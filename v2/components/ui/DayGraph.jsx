91086908fc561db2be20d4b4c0d66abe72e67c89
Huxley Marvit <huxmarv@nuevaschool.org>
2022-03-17 13:24:48 -0700

    remove tick marks omg finally the ticks are gone

--- v2/components/ui/DayGraph.jsx
+++ v2/components/ui/DayGraph.jsx
@@ -26,6 +26,7 @@ export const data = {
 	    label: [],
 	    data: [1, 1.2, 1.3, 1.4, 1.5],
 	    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+	    //backgroundColor: 'red',
 	    borderColor: 'rgba(255, 99, 132, 1)',
 	    borderWidth: 1,
 	},
@@ -43,6 +44,49 @@ export const options = {
 	    display: false,
 	},
     },
+    //gridLines: {
+    //    display: false
+    //},
+    //scale: {
+    //    ticks: {
+    //        display: false
+    //    }
+    //},
+    //scale: {
+    //    //x: {
+    //        ticks: {
+    //            // For a category axis, the val is the index so the lookup via getLabelForValue is needed
+    //            callback: function(val, index) {
+    //                // Hide every 2nd tick label
+    //                //return index % 2 === 0 ? this.getLabelForValue(val) : '';
+    //                return "whee"
+    //            },
+    //            color: 'red',
+    //        }
+    //    //}
+    //}
+
+    scales: {
+	r: {
+	    //angleLines: {
+		//display: false
+	    //},
+	    //suggestedMin: 50,
+	    //suggestedMax: 100
+	    ticks: {
+		display: false
+
+	    }
+	}
+    }
+
+
+    //scale: {
+    //    ticks: {
+    //        display: false,
+    //        maxTicksLimit: 3
+    //    }
+    //}
 };
 
 export default function DayGraph(props) {
