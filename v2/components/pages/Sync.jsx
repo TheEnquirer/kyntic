ff97039697e4dc5977f9157653256cd85afbb60c
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2022-05-02 16:38:45 -0700

    Should store data locally theoretically.

--- v2/components/pages/Sync.jsx
+++ v2/components/pages/Sync.jsx
@@ -6,13 +6,15 @@ import {
 	IonContent,
 	IonProgressBar,
 	IonIcon,
-	IonToast
+	IonToast,
+	IonList,
+	IonFooter
 } from '@ionic/react';
 import pageStyles from '../../styles/Pages.module.css';
 import { MetawearCapacitor } from 'metawear-capacitor';
 import React from 'react';
 
-//import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 class Sync extends React.Component {
 
@@ -24,9 +26,23 @@ class Sync extends React.Component {
 			connectCalled: false, // have we asked the plugin to connect?
 			connected: false, // have we been told by the plugin that we have successfully connected?
 			startedLogging: false, // have we started to log data? 
-			connectedListenerMade: false // have we made a listener that hears when we successfully connected?
+			connectedListenerMade: false, // have we made a listener that hears when we successfully connected?
+			accel: null, // last accel data point
+			gyro: null, // last gyro data point
+			path: null, // TODO: reset path to null once done logging
+			error: null
 		}
+		this.accelUpdated = false;
+		this.gyroUpdated = false;
     };
+
+	// ionic doesn't unmount tabs when they are switched, so this would not work
+	// async componentWillUnmount()
+	// {
+	// 	// cleanup time!
+	// 	// they'll have to reconnect to sensor
+	// 	await MetawearCapacitor.disconnect();
+	// }
 
 	/**
 	 * Creates a listener to see if we have successfully connected to the sensor.
@@ -64,17 +80,29 @@ class Sync extends React.Component {
 					}).catch(err => {
 						console.log("JS: Error while starting data logging:")
 						console.error(err);
-						// TODO: alert the user
+						this.setState({error: err.toString()})
+						setTimeout(() =>
+						{
+							this.setState({error: null})
+						}, 3000)
 					});
 				}
 				else {
 					console.log("JS: Error while making data files.");
-					// TODO: alert the user
+					this.setState({error: err.toString()})
+					setTimeout(() =>
+					{
+						this.setState({error: null})
+					}, 3000)
 				}
 			}).catch(err => {
 				console.log("JS: Error while making data files:")
 				console.error(err);
-				// TODO: alert the user
+				this.setState({error: err.toString()})
+				setTimeout(() =>
+				{
+					this.setState({error: null})
+				}, 3000)
 			});
 		}
 	}
@@ -84,8 +112,10 @@ class Sync extends React.Component {
 	 */
 	createGyroDataListener() {
 		MetawearCapacitor.addListener('gyroData', (gyro) => {
+			this.setState({gyro: gyro})
+			this.gyroUpdated = true
+			this.shouldWrite();
 			console.log(`JS: gyroData: (${gyro["x"]}, ${gyro["y"]}, ${gyro["z"]})`);
-			// TODO: display for user to see
 			// TODO: sync to server
 		});
 	}
@@ -95,43 +125,122 @@ class Sync extends React.Component {
 	 */
 	createAccelDataListener() {
 		MetawearCapacitor.addListener('accelData', (accel) => {
+			this.setState({accel: accel})
+			this.accelUpdated = true
+			this.shouldWrite();
 			console.log(`JS: accel: (${accel["x"]}, ${accel["y"]}, ${accel["z"]})`);
-			// TODO: display for user to see
 			// TODO: sync to server
 		});
 	}
 
-	// /**
-	//  * Unsuccessfully tries to write the data to the filesystem.
-	//  * @returns {Promise<WriteFileResult>}
-	//  */
-	// writeFile() {
-	// 	console.log("JS: gonna try to write fileeee")
-	// 	return Filesystem.writeFile({
-	// 		path: 'accel.txt',
-	// 		data: "test",
-	// 		directory: Directory.Documents,
-	// 		encoding: Encoding.UTF8,
-	// 	});
-	// };
+	/**
+	 * Should we write to the data file?
+	 */
+	shouldWrite() {
+		if (this.gyroUpdated && this.accelUpdated)
+		{
+			this.gyroUpdated = false, this.accelUpdated = false;
+			this.writeFile(this.state.accel, this.state.gyro)
+		}
+	}
+
+	createDataFile() {
+		let path = (new Date()).getTime().toString();
+		return Filesystem.writeFile({
+			path: path,
+			data: "!",
+			directory: Directory.Data,
+			encoding: Encoding.UTF8
+		}).then(() => {
+			this.setState({path: path})
+		})
+	}
+
+	/**
+	 * 
+	 * Write acceleration and gyroscope data to datafile.
+	 * @param {*} accel 
+	 * @param {*} gyro 
+	 */
+	async writeFile(accel, gyro) {
+		// data format: [(a_x,a_y,a_z):(g_x,g_y,g_z)];
+		// begginning of data file is a !
+		if (!this.state.path)
+		{
+			try{
+				await this.createDataFile();
+			} catch (e)
+			{
+				console.error(err);
+				this.setState({error: err.toString()})
+				setTimeout(() =>
+				{
+					this.setState({error: null})
+				}, 3000)
+			}
+		}
+		let data = `[(${accel["x"]},${accel["y"]},${accel["z"]}):(${gyro["x"]},${gyro["y"]},${gyro["z"]})];`
+		try {
+			Filesystem.appendFile({
+				path: path,
+				data: data,
+				directory: Directory.Data,
+				encoding: Encoding.UTF8
+			})
+		} catch (e) {
+			console.log(`Error while appending: ${e}`)
+		}
+	}
 
 	connectButton() {
 		if (!this.state.connectCalled) {
 			this.setState({connectCalled: true});
+			this.createDataFile();
 			MetawearCapacitor.connect()
 				.then(async () => {
 					console.log('Running connection did not error.');
 				})
 				.catch(err => {
 					console.error(err);
-					// TODO: alert the user that we couldn't connect
+					this.setState({error: err.toString()})
+					setTimeout(() =>
+					{
+						this.setState({error: null})
+					}, 3000)
 					this.setState({connectCalled: false});
 				});
 		}
 		this.createConnectedListener(); // listens to see if we have successfully connected
 	}
 
-	render() {
+	stopButton()
+	{
+		MetawearCapacitor.disconnect()
+			.then(async () => {
+				this.setState({connectCalled: false, connected: false, startedLogging: false, path: null})
+			})
+			.catch(err => {
+				console.error(err);
+				this.setState({error: err.toString()})
+				setTimeout(() =>
+				{
+					this.setState({error: null})
+				}, 3000)
+			})
+	}
+
+	render() 
+	{
+		let button;
+		if (this.state.startedLogging)
+		{
+			button = <IonButton onClick={() => this.stopButton()}>Stop</IonButton>
+		}
+		else
+		{
+			button = <IonButton onClick={() => this.connectButton()} expand='block'>Record<IonIcon slot="end" icon="bluetooth"></IonIcon></IonButton>
+		}
+
 		return (
 			<IonPage>
 				<IonToolbar>
@@ -147,10 +256,18 @@ class Sync extends React.Component {
 						<IonProgressBar type="indeterminate"></IonProgressBar>
 						// <ion-icon slot="end" name="bluetooth"></ion-icon>
 					}
-					<IonButton onClick={() => this.connectButton()} expand='block'>
-						Record 
-						<IonIcon slot="end" icon="bluetooth"></IonIcon>
-					</IonButton>
+					{button}
+					{this.state.gyro &&
+						<IonList>
+							Gyroscope: {this.state.gyro["x"]}, {this.state.gyro["y"]}, {this.state.gyro["z"]}
+						</IonList>
+					}
+					{this.state.accel &&
+						<IonList>
+							Acceleration: {this.state.accel["x"]}, {this.state.accel["y"]}, {this.state.accel["z"]}
+						</IonList>
+					}
+					{this.state.error && (<IonFooter><div className="absolute top-0 w-48 p-3 mt-12 font-bold text-center text-red-700 bg-red-300 rounded left-1/2 transform -translate-x-1/2 "> {this.state.error} </div></IonFooter>)}
 				</IonContent>
 			</IonPage>
 		);
